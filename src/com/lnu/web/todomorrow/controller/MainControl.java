c3bd272f10406cb4fa453575b400608f0cd08067
mwagnurr <mwagnurr@gmail.com>
2014-01-19 18:17:44 +0100

    changes layout in main.xhtml + small other changes

--- src/com/lnu/web/todomorrow/controller/MainControl.java
+++ src/com/lnu/web/todomorrow/controller/MainControl.java
@@ -1,11 +1,13 @@
 package com.lnu.web.todomorrow.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
 
 import com.lnu.web.todomorrow.dao.GoalDAOBean;
 import com.lnu.web.todomorrow.dao.TaskDAOBean;
@@ -20,6 +22,8 @@ public class MainControl {
 	@EJB
 	private TaskDAOBean taskDAO;
 
+	private Date date;
+
 	public MainControl() {
 	}
 
@@ -29,10 +33,30 @@ public class MainControl {
 
 		return bla;
 	}
-	
-	public String blabla(){
+
+	public String blabla() {
 		log("calling BLABALAAA");
 		return "BLABLA";
+	}
+
+	public void test1(ActionEvent actionEvent) {
+		log("test1 called");
+	}
+
+	public void test2(ActionEvent actionEvent) {
+		log("test2 called");
+	}
+
+	public void test3(ActionEvent actionEvent) {
+		log("test3 called");
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
 	private void log(String logMsg) {
